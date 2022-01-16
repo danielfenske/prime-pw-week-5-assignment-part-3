74e4f6e48e9920f7f0392ffa97fd9a013eec4772
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-15 23:55:23 -0600

    This commit tested the addToCollection function to ensure properties within 'album' object were correctly assigned and 'albums' were correctly added to collection array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -17,6 +17,32 @@ function addToCollection(title, artist, yearPublished) {
     artist: artist,
     yearPublished: yearPublished,
   };
-  collection.push(album); // adds object 'allbum' to end of 'collection' array
-  return album;
+  collection.push(album); // adds object 'album' to end of 'collection' array
+  return album; // Returns object 'album' whenever function is run
 } // end addToCollection
+
+
+// Test the addToCollection function by:
+  // Adding 6 albums to the 'collection' array
+  // Logging each album as added using the returned value
+  // After all albums are added, logging the 'collection' array
+
+// TEST: Logging each album as added using returned value
+
+console.log(addToCollection('Justice', 'Justin Bieber', '2021'));
+console.log(addToCollection('Donda', 'Kanye West', '2021'));
+console.log(addToCollection('Seasons Change', 'Scotty McCreery', '2018'));
+console.log(addToCollection('Uncomfortable', 'Andy Mineo', '2015'));
+console.log(addToCollection('Work in Progress', 'Andy Mineo', '2019'));
+console.log(addToCollection('Mmhmm', 'Reliant K', '2004'));
+
+// TEST: Log 'collection' array
+
+console.log(collection);
+// Statement output:
+  // 0: {title: 'Justice', artist: 'Justin Bieber', yearPublished: '2021'}
+  // 1: {title: 'Donda', artist: 'Kanye West', yearPublished: '2021'}
+  // 2: {title: 'Seasons Change', artist: 'Scotty McCreery', yearPublished: '2018'}
+  // 3: {title: 'Uncomfortable', artist: 'Andy Mineo', yearPublished: '2015'}
+  // 4: {title: 'Work in Progress', artist: 'Andy Mineo', yearPublished: '2019'}
+  // 5: {title: 'Mmhmm', artist: 'Reliant K', yearPublished: '2004'}
