78921e1be007bb1b1e3e5fb09789b60d92a37e06
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-15 23:46:10 -0600

    This commit created the function 'addToCollection', which allows the object 'album' to be added to the 'collection' array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,3 +3,20 @@ console.log('***** Music Collection *****')
 // Create a variable collection that starts as an empty array.
 
 let collection = [];
+
+// Add a function named 'addToCollection'. This function should:
+  // Take in the album's 'title', 'artist', and 'yearsPublished'
+  // Create a new object having the above properties
+  // Add the new object to the end of the 'collection' array
+  // Return the newly created object
+
+function addToCollection(title, artist, yearPublished) {
+  console.log('in addToCollection');
+  let album = { // object 'ablum' created to store 'title', 'artist', and 'yearsPublished' properties
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished,
+  };
+  collection.push(album); // adds object 'allbum' to end of 'collection' array
+  return album;
+} // end addToCollection
