4f749ca24196d778194092f42797798e4efb9783
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-16 00:10:18 -0600

    This commit created and tested the function 'showCollection' to list out the values within the 'collection' array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -5,14 +5,14 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 // Add a function named 'addToCollection'. This function should:
-  // Take in the album's 'title', 'artist', and 'yearsPublished'
+  // Take in the album's 'title', 'artist', and 'yearPublished'
   // Create a new object having the above properties
   // Add the new object to the end of the 'collection' array
   // Return the newly created object
 
 function addToCollection(title, artist, yearPublished) {
   console.log('in addToCollection');
-  let album = { // object 'ablum' created to store 'title', 'artist', and 'yearsPublished' properties
+  let album = { // object 'ablum' created to store 'title', 'artist', and 'yearPublished' properties
     title: title,
     artist: artist,
     yearPublished: yearPublished,
@@ -46,3 +46,29 @@ console.log(collection);
   // 3: {title: 'Uncomfortable', artist: 'Andy Mineo', yearPublished: '2015'}
   // 4: {title: 'Work in Progress', artist: 'Andy Mineo', yearPublished: '2019'}
   // 5: {title: 'Mmhmm', artist: 'Reliant K', yearPublished: '2004'}
+
+  // Add a function named 'showCollection'. This function should:
+    // Take in an array paramater.
+    // Console.log the number of items in the array.
+    // Loop over the array and console.log each album's information, formatted like: 'TITLE' by 'ARTIST', published in 'YEAR'.
+
+function showCollection (array) {
+  console.log('in showCollection');
+  console.log(array.length); // used to define number of values (or objects, 'albums') within array ('collection')
+  for (let index of array) { // for of loop used to loop through each index ('album') within the array ('collection')
+    console.log(`${index.title} by ${index.artist} in ${index.yearPublished}`);
+  } // end for of loop
+} // end showCollection
+
+// TEST the showCollection Function
+
+showCollection(collection);
+// Statement output:
+  // in showCollection
+  // 3-music-collection.js:57 6
+  // 3-music-collection.js:59 Justice by Justin Bieber in 2021
+  // 3-music-collection.js:59 Donda by Kanye West in 2021
+  // 3-music-collection.js:59 Seasons Change by Scotty McCreery in 2018
+  // 3-music-collection.js:59 Uncomfortable by Andy Mineo in 2015
+  // 3-music-collection.js:59 Work in Progress by Andy Mineo in 2019
+  // 3-music-collection.js:59 Mmhmm by Reliant K in 2004
