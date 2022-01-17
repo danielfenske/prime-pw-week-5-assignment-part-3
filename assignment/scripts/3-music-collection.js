fb678a9b6750a1777303a6cce41d14a5f5125d17
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-17 17:59:40 -0600

    This commit created and tested the function 'search', which takes an input and determines if there is a match within the 'collection' array

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -47,6 +47,8 @@ console.log(collection);
   // 4: {title: 'Work in Progress', artist: 'Andy Mineo', yearPublished: '2019'}
   // 5: {title: 'Mmhmm', artist: 'Reliant K', yearPublished: '2004'}
 
+
+
   // Add a function named 'showCollection'. This function should:
     // Take in an array paramater.
     // Console.log the number of items in the array.
@@ -73,6 +75,8 @@ showCollection(collection);
   // Work in Progress by Andy Mineo in 2019
   // Mmhmm by Reliant K in 2004
 
+
+
 // EXTRA PRACTICE: Pull all artists from the 'collection' array
   // function was created to help me understand how to pull properties from objects within array
 
@@ -85,6 +89,7 @@ showCollection(collection);
   } // end getArtist
 
   console.log(getArtists(collection, 'artist'));
+
 
 
 // Add a function named 'findByArtist'. This function should:
@@ -117,3 +122,45 @@ console.log(findByArtist('Andy Mineo'));
     // length: 1
 
   console.log(findByArtist('Taylor Switft')); // Statement output: returns empty array ([])
+
+
+
+// Create a function called 'search'. This function should:
+  // Take an input peramter for a search criteria object. Create your solution based on a search object that has these properties.
+  // The returned output from 'search' should meet these requirements:
+    // Return a new array of all items in the 'collection' matching all of the search criteria (use conditional + &&)
+    // If no results are found, return an empty array
+    // If there is no search object or an empty search object provided as input, then return all albums in the 'collection'
+
+// EXTRA PRACTICE: Learning to see how a function recieves an object as an input
+
+// function test(object){
+//   console.log('in test');
+//   return object;
+// } // end test
+//
+// console.log(test({artist:'Ray Charles', year: 1957}));
+
+function search(object){
+  console.log('in search');
+
+  let searchResults = []; // if object is found within 'collection', it will be pushed to this empty array
+
+  if (object === undefined || Object.keys(object) == 0)  {
+    return collection; // will return 'collection' array if there's no search object or an empty serach object provided as input
+  }
+
+  for (let i=0; i<collection.length; i++) { // loops through 'collection' array
+    if (collection[i].artist === object.artist && collection[i].yearPublished == object.year) { // object must match the artist and year keys within 'collection' array
+      searchResults.push(collection[i]); // if conditions are met, the album will be added to empty 'output' array
+      }
+  } return searchResults; // function will return 'searchResults' array -- will include album if criteria is met or empty array if there's no match
+} // end search
+
+console.log(search()); // Statement output: 'collection' array
+
+console.log(search({})); // Statement output: 'collection' array
+
+console.log(search({artist: 'Andy Mineo', year: 2015})); // Statement output: {title: 'Uncomfortable', artist: 'Andy Mineo', yearPublished: '2015'}
+
+console.log(search({artist: 'Taylor Swift', year: 2017})); // Statement output: [] (empty array)
